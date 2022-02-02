49544158721942d37e16b77f3044f123440d8a01
CCGCAM <camicar>
2022-02-02 09:32:42 +0100

    updated

--- R/Correlated_value.R
+++ R/Correlated_value.R
@@ -14,5 +14,24 @@ correlatedValue = function(x, r){
   SD = sqrt(ve)
   e  = rnorm(length(x), mean=0, sd=SD)
   y  = r*x + e
+  ##check if some values is negative
+  for (i in c(1:length(y))){
+    if (y[i] <=0) {
+      y[i]=0
+    }
+  }
+  
   return(y)
 }
+
+######### Another option could be based on the relationship between Cab-Car
+# constants from ANGERS03 Leaf Optical Data
+#slope = 0.2234
+#intercept = 0.9861
+#spread = 4.6839
+#grid_var=data.LUT$Cab
+# car_lin = slope * grid_var+ intercept
+# lower_car = slope / spread * 3 * grid_var
+# upper_car = slope * spread / 3 * grid_var + 2 * intercept
+# 
+# car_noise=rnorm(length(grid_var), mean=3, sd=0.5)+ car_lin
