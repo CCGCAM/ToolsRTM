e3769d5d741e9fe77296b7f8f980ba1556c14c42
CCGCAM <camicar>
2022-02-09 16:50:55 +0100

    updated

--- R/Compute_BRF.R
+++ R/Compute_BRF.R
@@ -12,18 +12,27 @@
 #' @param rdot numeric. Hemispherical-directional reflectance factor in viewing direction
 #' @param rsot numeric. Bi-directional reflectance factor
 #' @param tts numeric. Solar zenith angle
-#' @param SpecATM_Sensor list. direct and diffuse radiation for clear conditions
+#' @param data.light list. direct and diffuse radiation for clear conditions, is NULL use default values
 #' @return BRF numeric. Bidirectional reflectance factor
 #' @export
 #' 
-Compute_BRF  <- function(rdot=NULL,rsot=NULL,tts=NULL,SpecATM_Sensor=NULL){
+Compute_BRF  <- function(rdot=NULL,rsot=NULL,tts=NULL,data.light=NULL){
   
   ############################## #
   ##	direct / diffuse light	##
   ############################## #
-  Es <- SpecATM_Sensor$direct_light ##ToolsRTM::dataSpec_PDB[,11]
-  Ed <- SpecATM_Sensor$diffuse_light ##ToolsRTM::dataSpec_PDB[,12]
-  rd <- pi/180
+  if (is.null(data.light)){
+    Es <- ToolsRTM::dataSpec_PDB[,11]
+    Ed <- ToolsRTM::dataSpec_PDB[,12]
+    rd <- pi/180
+    
+  } else{
+    Es <- data.light$direct_light ##
+    Ed <- data.light$diffuse_light ##
+    rd <- pi/180
+   
+  }
+
   #
   # if (skyl == 0.1){
   #   #  by default skyl = 0.1
