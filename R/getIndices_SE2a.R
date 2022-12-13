a145b4bdb20638ff8a7d8a4ea16250c893a68968
CCGCAM <camicar>
2022-12-13 16:49:58 +0100

    add Se2a and b

--- R/getIndices_SE2a.R
+++ R/getIndices_SE2a.R
@@ -2,7 +2,8 @@
 #' extract spectral indices at Sentinel-2 resolution
 #'
 #' @param df a dataframe with reflectance where each rows correspond with an spectrum 
-#' @param wavelengths  wavelent of each reflectance
+#' @param wavelengths  wavelength ovector with each reflectance
+#' @sensor Sensor options: 'Sentinel-2a', or 'Sentinel-2b'
 #' @param df.data  dataset with IDs that corresponde with each spectrum, is null is also enable
 #' @param header TRUE organize the indices by caterioas / False only return then name
 #'
@@ -11,24 +12,46 @@
 #'
 #' @examples
 #' 
-getIndicesSE2a <- function(df, wavelengths,df.data=NULL, header = F) {
+getIndicesSE2a <- function(df, wavelengths,sensor='Sentinel-2a',df.data=NULL ,header = F) {
   
-  range2interpo = as.numeric(gsub(".*?([0-9]+).*", "\\1", wavelengths))
- 
+  if (class(data)[1] == "matrix"){
+    df<-as.data.frame(df)
+  }
+  
+  #df=data.field[,bands.rfl];wavelengths=wave.se2.filtered;df.data=data.field;header = F
+  wave.avalaible = as.numeric(gsub(".*?([0-9]+).*", "\\1", wavelengths))
 
-  ##original conf form J.B Feret for CR_SWIR
-  S2a_Bands <- c('B2'=496.6, 'B3'=560.0, 'B4'=664.5, 'B5'=703.9, 'B6'=740.2,
-               'B7' = 782.5, 'B8' = 835.1, 'B8A' = 864.8, 'B11' = 1613, 'B12' = 2202)
+  
+  ##original conf form J.B Feret for CR.SWIR
+  bandset.SE2a <- c('B1'=442.7,'B2'=492.7, 'B3'=559.8, 'B4'=664.6, 'B5'=704.1, 'B6'=740.5,
+                 'B7' = 782.8, 'B8' =832.8,'B8A' = 864.7, 
+                 'B9'= 945.1,'B10'=1373.5,'B11' = 1613.7 ,'B12' = 2202.4)
+  
+  bandset.SE2b <- c('B1'=442.2,'B2'=492.3, 'B3'=558.9, 'B4'=664.9, 'B5'=703.8, 'B6'=739.1,
+                 'B7' =779.7, 'B8' =  832.9, 'B8A' = 864.0, 
+                 'B9'= 943.2,'B10'=1376.9,'B11'= 1610.4,'B12' =2185.7 )
+  
+  
+  if (is.null(sensor) == TRUE){
+    bandset.SE<-  bandset.SE2a
+  } else if (sensor == 'Sentinel-2a') {
+    bandset.SE<-  bandset.SE2a
+  } else if (sensor == 'Sentinel-2a'){
+    bandset.SE<-  bandset.SE2b
+  }
+    
+
   indices.list = list()
   # create progress bar
   total=dim(df)[1]
   barProgress <- txtProgressBar(min = 1, max = total, style = 3)
-  
+
   for (i in c(1:dim(df)[1])){
   
-  values<-df[i,]
+  
   r = values<-df[i,]
-  names(r) <- range2interpo
+  r = signal::interp1(wave.avalaible, as.numeric(r), bandset.SE, extrap = T)
+  names(r) <- names(bandset.SE)
 
  
 
@@ -38,51 +61,51 @@ getIndicesSE2a <- function(df, wavelengths,df.data=NULL, header = F) {
     
     # NDVI (R800-R670)/(R800+R670)
     # Rouse et al. (1974)
-    indices['NDVI'] <- (r['832'] - r['664']) / (r['832'] + r['664'])
+    indices['NDVI'] <- (r['B8'] - r['B4']) / (r['B8'] + r['B4'])
     
     # RDVI (R800-R670)/(R800+R670)^0.5
     # Rougean and Breon (1995)
-    indices['RDVI'] <- (r['832'] - r['664']) / (r['832'] + r['664']) ** 0.5
+    indices['RDVI'] <- (r['B8'] - r['B4']) / (r['B8'] + r['B4']) ** 0.5
     
     # SR R800/R670
     # Jordan (1969)
-    indices['SR'] <- r['832'] / r['664']
+    indices['SR'] <- r['B8'] / r['B4']
     
     # MSR (R800/R670-1)/((R800/R670)^0.5+1)
     # Chen (1996)
-    indices['MSR'] <- (r['832'] / r['664'] - 1) / ((r['832'] / r['664']) ** 0.5 + 1)
+    indices['MSR'] <- (r['B8'] / r['B4'] - 1) / ((r['B8'] / r['B4']) ** 0.5 + 1)
     
     # OSAVI [(1+0.16)*(R800-R670)/(R800+R670+0.16)]
     # Rondeaux et al. (1996)
-    indices['OSAVI'] <- ((1 + 0.16) * (r['832'] - r['664']) / (r['832'] + r['664'] + 0.16))
+    indices['OSAVI'] <- ((1 + 0.16) * (r['B8'] - r['B4']) / (r['B8'] + r['B4'] + 0.16))
     
     # MSAVI 1/2*[(2*R800+1-?((?(2*R800+1)?^2)-8*(R800-R670))]
     # Qi et al. (1994)    
-    indices['MSAVI'] <- 1 / 2 * (2 * r['832'] + 1 - sqrt(((2 * r['832'] + 1) ^ 2) - 8 * (r['832'] - r['664'])))
+    indices['MSAVI'] <- 1 / 2 * (2 * r['B8'] + 1 - sqrt(((2 * r['B8'] + 1) ^ 2) - 8 * (r['B8'] - r['B4'])))
     
     # MTVI1 1.2*[1.2*(R800-R550)-2.5*(R670-R550)]
     # Broge & Leblanc (2000); Haboudane et al. (2004)
-    indices['MTVI1'] <- 1.2 * (1.2 * (r['832'] - r['559']) - 2.5 * (r['664'] - r['559']))
+    indices['MTVI1'] <- 1.2 * (1.2 * (r['B8'] - r['B3']) - 2.5 * (r['B4'] - r['B3']))
     
     # MTVI2 (1.5*[1.2*(R800-R550)-2.5*(R670-R550)])/SQR((2*R800+1)^2-(6*R800-5*SQR(R670))-0.5)
     # Haboudane et al. (2004)    
-    indices['MTVI2'] <- (1.5 * (1.2 * (r['832'] - r['559']) - 2.5 * (r['664'] - r['559']))) / sqrt((2 * r['832'] + 1) ^ 2 - (6 * r['832'] - 5 * sqrt(r['664'])) - 0.5)
+    indices['MTVI2'] <- (1.5 * (1.2 * (r['B8'] - r['B3']) - 2.5 * (r['B4'] - r['B3']))) / sqrt((2 * r['B8'] + 1) ^ 2 - (6 * r['B8'] - 5 * sqrt(r['B4'])) - 0.5)
     
-    # MCARI ((R700-R670) - 0.2*(R700-R550))*(R700/R670)
+    # MCARI ((RB5-R670) - 0.2*(RB5-R550))*(RB5/R670)
     # Hermann et al. (2010)
-    indices['MCARI'] <- ((r['704'] - r['664']) - 0.2 * (r['704'] - r['559'])) * (r['704'] / r['664'])
+    indices['MCARI'] <- ((r['B5'] - r['B4']) - 0.2 * (r['B5'] - r['B3'])) * (r['B5'] / r['B4'])
     
     # MCARI1 1.2* [2.5* (R800-  R670)-  1.3* (R800 -R550) ]
     # Haboudane et al. (2004)
-    indices['MCARI1'] <- 1.5 * (2.5 * (r['832'] - r['664']) - 1.3 * (r['832'] - r['559']))
+    indices['MCARI1'] <- 1.5 * (2.5 * (r['B8'] - r['B4']) - 1.3 * (r['B8'] - r['B3']))
     
     # MCARI2 (1.5*[2.5*(R800-R670)-1.3*(R800-R550) ])/SQRT((2*R800+1)^2-(6*R800-5*SQRT(R670))-0.5)
     # Haboudane et al. (2004)  
-    indices['MCARI2'] <- (1.5 * (2.5 * (r['832'] - r['664']) - 1.3 * (r['832'] - r['559']))) / sqrt((2 * r['832'] + 1) ^ 2 - (6 * r['832'] - 5 * sqrt(r['664'])) - 0.5)
+    indices['MCARI2'] <- (1.5 * (2.5 * (r['B8'] - r['B4']) - 1.3 * (r['B8'] - r['B3']))) / sqrt((2 * r['B8'] + 1) ^ 2 - (6 * r['B8'] - 5 * sqrt(r['B4'])) - 0.5)
     
     # EVI 2.5*(R800-R670)/(R800+6*R670-7.5*R400+1)
     # Huete et al. (2002)    
-    indices['EVI'] <- 2.5 * (r['832'] - r['664']) / (r['832'] + 6 * r['664'] - 7.5 * r['492'] + 1)
+    indices['EVI'] <- 2.5 * (r['B8'] - r['B4']) / (r['B8'] + 6 * r['B4'] - 7.5 * r['B2'] + 1)
 
     ## Pigmentos
     if(header) indices['Pigments'] <- 'Pigments'
@@ -90,15 +113,15 @@ getIndicesSE2a <- function(df, wavelengths,df.data=NULL, header = F) {
 
     # GM1 R750/R550
     # Gitelson and Merzlyak (1997)
-    indices['GM1'] <- r['740'] / r['559']
+    indices['GM1'] <- r['B6'] / r['B3']
     
-    # GM2 R750/R700
+    # GM2 R750/RB5
     # Gitelson and Merzlyak (1997)
-    indices['GM2'] <- r['740'] / r['704']
+    indices['GM2'] <- r['B6'] / r['B5']
     
-    # TCARI 3*[(R700-R670)-0.2*(R700-R550)*(R700/R670)]
+    # TCARI 3*[(RB5-R670)-0.2*(RB5-R550)*(RB5/R670)]
     # Haboudane et al. (2002)
-    indices['TCARI'] <- 3 * ((r['704'] - r['664']) - 0.2 * (r['704'] - r['559']) * (r['704'] / r['664']))
+    indices['TCARI'] <- 3 * ((r['B5'] - r['B4']) - 0.2 * (r['B5'] - r['B3']) * (r['B5'] / r['B4']))
     
     # TCARI/OSAVI TCARI/OSAVI
     # Haboudane et al. (2002)
@@ -106,86 +129,79 @@ getIndicesSE2a <- function(df, wavelengths,df.data=NULL, header = F) {
     
     # TVI 0.5*[120*(R750-R550)-200*(R670-R550) ]
     # Broge and Leblanc (2000)
-    indices['TVI'] <- 0.5 * (120 * (r['740'] - r['559']) - 200 * (r['664'] - r['559']))
+    indices['TVI'] <- 0.5 * (120 * (r['B6'] - r['B3']) - 200 * (r['B4'] - r['B3']))
     # SRPI R430/R680
-    
-    if (is.null(wavelengths) | length(wavelengths) == 12  | length(wavelengths) == 13 ) {
-      # SIPI (R800-R445)/(R800+R680)
-      # Pe??uelas et al. (1995)
-      indices['SIPI'] <- (r['832'] - r['442']) / (r['832'] + r['664'])
-    } else{
+    if(any(bandset.SE <= 445)){
       
-    }
-
-
+    # SIPI (R800-R445)/(R800+R680)
+      # Pe??uelas et al. (1995)
+    indices['SIPI'] <- (r['B8'] - r['B1']) / (r['B8'] + r['B4'])
+    } 
 
     ## SEntinel 2a
     if(header) indices['SE2a'] <- 'SE2a Indices'
     
     #Anthocyanin reflectance index
-    indices['ARI'] <- (1/r['559'])- (1/r['704']) 
-    indices['GNDVI'] <- (r['832'] - r['559']) / (r['832'] + r['559'])
-    indices['CIg'] <- r['832'] / r['559'] -1 
+    indices['ARI'] <- (1/r['B3'])- (1/r['B5']) 
+    indices['GNDVI'] <- (r['B8'] - r['B3']) / (r['B8'] + r['B3'])
+    indices['CIg'] <- r['B8'] / r['B3'] -1 
     y = 0.069;
-    indices['ARVI'] <- (r['864'] - r['832'] - y * (r['664'] -r['492']) ) / (r['864'] + r['664'] - y * (r['664'] -r['492']) )
+    indices['ARVI'] <- (r['B8A'] - r['B8'] - y * (r['B4'] -r['B2']) ) / (r['B8A'] + r['B4'] - y * (r['B4'] -r['B2']) )
     
-    if (is.null(wavelengths) | length(wavelengths) == 12  | length(wavelengths) == 13 ) {
-      indices['AVI'] <- 2.0 * r['945'] - r['664']
-    } else{
-  
-    }
+    if (any(bandset.SE == 945) | any(bandset.SE == 943)) {
+      indices['AVI'] <- 2.0 * r['B9'] - r['B4']
+    } 
     #Atmospherically Resistant Vegetation Index 2  (abbrv. ARVI2)
-    indices['ARV2'] <- -0.18 + 1.17 *(r['832'] - r['664']) / (r['832'] + r['664'])
+    indices['ARV2'] <- -0.18 + 1.17 *(r['B8'] - r['B4']) / (r['B8'] + r['B4'])
     #Normalized Difference NIR/SWIR Normalized Burn Ratio (abbrv. NBR)
-    indices['NBR'] <- (r['832'] - r['2202']) / (r['832'] + r['2202'])
-    indices['NBR.2'] <- (r['1613'] - r['2202']) / (r['1613'] + r['2202'])
+    indices['NBR'] <- (r['B8'] - r['B12']) / (r['B8'] + r['B12'])
+    indices['NBR.2'] <- (r['B11'] - r['B12']) / (r['B11'] + r['B12'])
     
     #Normalized Difference NIR/Rededge Normalized Difference Red-Edge (abbrv. NDRE)
-    indices['NDRE'] <- (r['832'] - r['704']) / (r['832'] + r['704'])
+    indices['NDRE'] <- (r['B8'] - r['B5']) / (r['B8'] + r['B5'])
     #Normalized Difference NIR/MIR Modified Normalized Difference Vegetation Index (abbrv. MNDVI)
-    indices['MNDVI'] <- (r['832'] - r['1613']) / (r['832'] + r['1613'])
+    indices['MNDVI'] <- (r['B8'] - r['B11']) / (r['B8'] + r['B11'])
     
     #Red edge 1  (abbrv. Rededge1)
-    indices['RedEg1'] <- r['704'] / r['664']
-    indices['RedEg2'] <- (r['704'] - r['664']) / (r['704'] + r['664'])
+    indices['RedEg1'] <- r['B5'] / r['B4']
+    indices['RedEg2'] <- (r['B5'] - r['B4']) / (r['B5'] + r['B4'])
     #Wide Dynamic Range Vegetation Index  (abbrv. WDRVI)
-    indices['WDRVI'] <- (0.1 * r['832'] - r['664']) / (0.1 * r['832'] + r['664'])
+    indices['WDRVI'] <- (0.1 * r['B8'] - r['B4']) / (0.1 * r['B8'] + r['B4'])
     #Normalized Difference Water Index
-    indices['NDWI'] <- (r['864'] - r['1613']) / (r['864'] + r['1613'])
-    indices['NDWI2'] <- (r['864'] - r['2202']) / (r['864'] + r['2202'])
+    indices['NDWI'] <- (r['B8A'] - r['B11']) / (r['B8A'] + r['B11'])
+    indices['NDWI2'] <- (r['B8A'] - r['B12']) / (r['B8A'] + r['B12'])
     
     #Leaf Water Content Index  (abbrv. LWCI)
-    #MIDIR = r['1613']
-    #indices['LWCI'] <- log(1.0 -( r['832'] - r['1613'])) / -log(1-0 * (r['832'] - r['1613']))
+    MIDIR = 0.101
+    indices['LWCI'] <- log(1.0 -( r['B8'] - MIDIR)) / -log(1.0 * (r['B8'] - MIDIR))
     #CR_SWIR from J.B.Feret
-    indices['CR.SWIR'] <- r['1613']/(r['864']+(S2a_Bands['B11']-S2a_Bands['B8A'])*(r['2202']-r['864'])/(S2a_Bands['B12']-S2a_Bands['B8A']))
+    indices['CR.SWIR'] <- r['B11']/(r['B8A']+(bandset.SE['B11']-bandset.SE['B8A'])*(r['B12']-r['B8A'])/(bandset.SE['B12']-bandset.SE['B8A']))
 
     #CIre
-    indices['CIre'] <- (r['782'] / r['704'])-1
-    indices['CIgreen'] <- (r['832'] / r['559'])-1
-    indices['IRECI'] <- (r['782'] - r['664']) / (r['704'] / r['740'])
-    indices['S2REP'] <- 700 + 35*( ((r['782'] - r['664']/2) -  r['704'])/ (r['740'] - r['704']))
-    indices['RVI'] <-  (r['832'] / r['664'])
+    indices['CIre'] <- (r['B7'] / r['B5'])-1
+    indices['CIgreen'] <- (r['B8'] / r['B3'])-1
+    indices['IRECI'] <- (r['B7'] - r['B4']) / (r['B5'] / r['B6'])
+    indices['REP'] <- 700 + 40* (((r['B4'] + r['B7'])/2) -  r['B5'])/ (r['B6'] - r['B5'])
+    indices['RVI'] <-  (r['B8'] / r['B4'])
     #Perpendicular Vegetation Index 
     #Initialize parameters
     a = 0.149
     ar = 0.374
     b = 0.735
-    indices['PVI'] <-  (1.0 /sqrt(a** 2.0+ 1.0)) * (r['832'] - ar - b)
+    indices['PVI'] <-  (1.0 /sqrt(a** 2.0+ 1.0)) * (r['B8'] - ar - b)
     #Red-Edge Inflection Point 1  (abbrv. REIP1)
-    indices['REIP1'] <- 700 + 405 * ( ((r['664'] - r['782']/2) -  r['704'])/ (r['740'] - r['704']))
-    indices['REIP2'] <- 700 + 405 * ( ((r['664'] - r['782']/2) -  r['704'])/ (r['740'] - r['704']))
-    
+    indices['REIP1'] <- 700.0 + 40.0  * (( ((r['B4'] + r['B7'])/2) -  r['B5'])/ (r['B6'] - r['B5']))
+    indices['REIP2'] <- 702.0 + 40.0  * (( ((r['B4'] + r['B7'])/2) -  r['B5'])/ (r['B6'] - r['B5']))
     ## BGR
     if(header) indices['BGR'] <- 'BGR'
     
     # G R550/R670
     # -
-    indices['Greeness'] <- r['559'] / r['664']
+    indices['Greeness'] <- r['B3'] / r['B4']
     
-    # R R700/R670
+    # R RB5/R670
     # Gitelson et al. (2000)
-    indices['Redness'] <- r['704'] / r['664']
+    indices['Redness'] <- r['B5'] / r['B4']
     
     # RARS R746/R513
     
@@ -194,29 +210,31 @@ getIndicesSE2a <- function(df, wavelengths,df.data=NULL, header = F) {
     
     # PSSRa R800/R680
     # Blackburn (1998)    
-    indices['PSSRa'] <- r['832'] / r['664']
+    indices['PSSRa'] <- r['B8'] / r['B4']
    
     if(header) indices['Red-edge'] <- 'Red-edge'
-      if(any(range2interpo == 864)){
+      if(any(bandset.SE == 864) | any(bandset.SE == 865)){
         
-        indices['SIFe1'] <- r['740']  / (r['864'] + (bandsSE['B6'] - bandsSE['B8A']) * (r['782'] - r['864']) / (bandsSE['B7'] - bandsSE['B8A']))
-        indices['SIFe2'] <- r['704']  / (r['782'] + (bandsSE['B5'] - bandsSE['B7']) * (r['740'] - r['782']) / (bandsSE['B6'] - bandsSE['B7']))
-        indices['SIFe3'] <- r['704']  / (r['864'] + (bandsSE['B5'] - bandsSE['B8A']) * (r['740'] - r['864']) / (bandsSE['B6'] - bandsSE['B8A']))
-        indices['SIFe4'] <- r['665']  / (r['864'] + (bandsSE['B4'] - bandsSE['B8A']) * (r['704'] - r['864']) / (bandsSE['B6'] - bandsSE['B8A']))
-        indices['SIFe5'] <- r['740']  / (r['864'] + (bandsSE['B6'] - bandsSE['B8A']) * (r['704'] - r['864']) / (bandsSE['B5'] - bandsSE['B8A']))
-        indices['SIFe6'] <- r['782']  / (r['864'] + (bandsSE['B7'] - bandsSE['B8A']) * (r['704'] - r['864']) / (bandsSE['B5'] - bandsSE['B8A']))
-        indices['SIFe7'] <- r['762']  / (r['864'] + (bandsSE['B.762'] - bandsSE['B8A']) * (r['704'] - r['864']) / (bandsSE['B5'] - bandsSE['B8A']))
-      
+        indices['SIFe1'] <- r['B6']  / (r['B8A'] + (bandset.SE['B6'] - bandset.SE['B8A']) * (r['B7'] - r['B8A']) / (bandset.SE['B7'] - bandset.SE['B8A']))
+        indices['SIFe2'] <- r['B5']  / (r['B7'] + (bandset.SE['B5'] - bandset.SE['B7']) * (r['B6'] - r['B7']) / (bandset.SE['B6'] - bandset.SE['B7']))
+        indices['SIFe3'] <- r['B5']  / (r['B8A'] + (bandset.SE['B5'] - bandset.SE['B8A']) * (r['B6'] - r['B8A']) / (bandset.SE['B6'] - bandset.SE['B8A']))
+        indices['SIFe4'] <- r['B4']  / (r['B8A'] + (bandset.SE['B4'] - bandset.SE['B8A']) * (r['B5'] - r['B8A']) / (bandset.SE['B6'] - bandset.SE['B8A']))
+        indices['SIFe5'] <- r['B6']  / (r['B8A'] + (bandset.SE['B6'] - bandset.SE['B8A']) * (r['B5'] - r['B8A']) / (bandset.SE['B5'] - bandset.SE['B8A']))
+        indices['SIFe6'] <- r['B7']  / (r['B8A'] + (bandset.SE['B7'] - bandset.SE['B8A']) * (r['B5'] - r['B8A']) / (bandset.SE['B5'] - bandset.SE['B8A']))
+        
       } else {
-        indices['SIFe1'] <- r['740']  / (r['832'] + (bandsSE['B6'] - bandsSE['B8']) * (r['782'] - r['832']) / (bandsSE['B7'] - bandsSE['B8']))
-        indices['SIFe2'] <- r['704']  / (r['782'] + (bandsSE['B5'] - bandsSE['B7']) * (r['740'] - r['782']) / (bandsSE['B6'] - bandsSE['B7']))
-        indices['SIFe3'] <- r['704']  / (r['832'] + (bandsSE['B5'] - bandsSE['B8']) * (r['740'] - r['832']) / (bandsSE['B6'] - bandsSE['B8']))
-        indices['SIFe4'] <- r['665']  / (r['832'] + (bandsSE['B4'] - bandsSE['B8']) * (r['704'] - r['832']) / (bandsSE['B6'] - bandsSE['B8']))
-        indices['SIFe5'] <- r['740']  / (r['832'] + (bandsSE['B6'] - bandsSE['B8']) * (r['704'] - r['832']) / (bandsSE['B5'] - bandsSE['B8']))
-        indices['SIFe6'] <- r['782']  / (r['832'] + (bandsSE['B7'] - bandsSE['B8']) * (r['704'] - r['832']) / (bandsSE['B5'] - bandsSE['B8']))
-        indices['SIFe7'] <- r['762']  / (r['832'] + (bandsSE['B.762'] - bandsSE['B8']) * (r['704'] - r['832']) / (bandsSE['B5'] - bandsSE['B8']))
-      }  
-
+        indices['SIFe1'] <- r['B6']  / (r['B8'] + (bandset.SE['B6'] - bandset.SE['B8']) * (r['B7'] - r['B8']) / (bandset.SE['B7'] - bandset.SE['B8']))
+        indices['SIFe2'] <- r['B5']  / (r['B7'] + (bandset.SE['B5'] - bandset.SE['B7']) * (r['B6'] - r['B7']) / (bandset.SE['B6'] - bandset.SE['B7']))
+        indices['SIFe3'] <- r['B5']  / (r['B8'] + (bandset.SE['B5'] - bandset.SE['B8']) * (r['B6'] - r['B8']) / (bandset.SE['B6'] - bandset.SE['B8']))
+        indices['SIFe4'] <- r['B4']  / (r['B8'] + (bandset.SE['B4'] - bandset.SE['B8']) * (r['B5'] - r['B8']) / (bandset.SE['B6'] - bandset.SE['B8']))
+        indices['SIFe5'] <- r['B6']  / (r['B8'] + (bandset.SE['B6'] - bandset.SE['B8']) * (r['B5'] - r['B8']) / (bandset.SE['B5'] - bandset.SE['B8']))
+        indices['SIFe6'] <- r['B7']  / (r['B8'] + (bandset.SE['B7'] - bandset.SE['B8']) * (r['B5'] - r['B8']) / (bandset.SE['B5'] - bandset.SE['B8']))
+            }  
+    
+    indices['SBI'] <- 0.3037*r['B2']+0.2793*r['B3']+0.4743*r['B4']+0.5585*r['B8']+0.5082*r['B11']+0.1863*r['B2'] 
+    indices['GVI'] <- -0.2848*r['B2'] - 0.2435*r['B3']  - 0.5436*r['B4']+0.7243*r['B8']+0.0840*r['B11'] - 0.1800*r['B12'] 
+    indices['WET'] <- 0.1509*r['B2']+0.1973*r['B3']+0.3279*r['B4']+0.3406*r['B8A']-0.7112*r['B11']-0.4572*r['B12'] 
+    
     indices.list[[i]] = indices
     setTxtProgressBar(barProgress, i)
     
@@ -224,18 +242,18 @@ getIndicesSE2a <- function(df, wavelengths,df.data=NULL, header = F) {
  
   df.indices <- data.frame(matrix(unlist(indices.list), nrow=length(indices.list), byrow=T))
   colnames(df.indices)<-names(indices)
-  df.rfl<-as.data.frame(df)
-  colnames(df.rfl)<-paste0('RFL.',wavelengths,sep='')
-  
   if (is.null(df.data)){
     ## remove indices wih no data
     df.indices = df.indices[, colSums(is.na(df.indices)) != nrow(df.indices)]
+    df.indices <- df.indices[!is.infinite(colSums(df.indices)),]
+
     df.indices_<-df.indices
 
  
   } else{
     ## remove indices wih no data
     df.indices = df.indices[, colSums(is.na(df.indices)) != nrow(df.indices)]
+    df.indices <- df.indices[!is.infinite(colSums(df.indices)),]
     df.indices_<-cbind(df.data,df.indices)
     
   }
