42bb3d0d786f62ff0489f4fcc29a5263c59eb04a
CCGCAM <camicar>
2023-04-17 09:28:45 +0200

    fixed bug in infinite values

--- R/getSeries.R
+++ R/getSeries.R
@@ -10,10 +10,10 @@
 #' @export
 #'
 #' @examples
-#' 
+#'
 getSeries<-function(pathRaster=NULL, shapefile=NULL, band_names=NULL,factorR=NULL,get.indices = T){
   options(warn=-1) ###avoid warnings
- 
+
   if (is.null(factorR)){
     message('please provide factor scale for reflectance bands')
     stop()
@@ -29,18 +29,18 @@ getSeries<-function(pathRaster=NULL, shapefile=NULL, band_names=NULL,factorR=NUL
     stop('TIF format is needed')
   } else {
       message('processing rasters ...')
-    
+
       ##################################################################################
-    
+
       progress_bar = txtProgressBar(min=0, max=length(files), style = 3, char="=")
       list.indices<-list()
-      for (k in c(1:length(files))){ 
+      for (k in c(1:length(files))){
         setTxtProgressBar(progress_bar, k)
-        #rs <- stack(files) 
+        #rs <- stack(files)
         ## by order
         rs <- brick(files[k])
         names(rs) <- band_names
-  
+
         if (class(shapefile)[1] == 'SpatialPointsDataFrame'){
           r.extract <- raster::extract(rs, shapefile, df = T, na.rm = T, cellnumbers = F)
           data.write<-data.frame(ID=r.extract[,'ID'],Date= dates[k],r.extract[,c(2:dim(r.extract)[2])]* factor)
@@ -50,19 +50,19 @@ getSeries<-function(pathRaster=NULL, shapefile=NULL, band_names=NULL,factorR=NUL
           data.write<-data.frame(ID=r.extract[,'ID'],cell = r.extract[,'cell'],Date= dates[k],r.extract[,c(3:dim(r.extract)[2])]* factor)
           se2.bands<-names(data.write[,grep(colnames(data.write),pattern="B",fixed = TRUE)])
         }
-        
+
         #r.extract[is.nan(r.extract)] <- NA
-      
+
         if (is.null(get.indices) | get.indices == F){
-          print('Hello')
-          
+
           list.indices[[k]]<- data.write
+        } else {
+          message(cat(' '))
+          message(paste('adding spectral indices for the SE-2 image: ',k,'/',length(files),sep = ''))
+          list.indices[[k]]<-ToolsRTM::getIndicesSE2(df=data.write,sensor='Sentinel-2a', df.data=data.write)
+
         }
-        message(cat(' '))
-        
-        message(paste('adding spectral indices for the SE-2 image: ',k,'/',length(files),sep = ''))
-        list.indices[[k]]<-ToolsRTM::getIndicesSE2(df=data.write,sensor='Sentinel-2a', df.data=data.write)  
-       
+
       }
       ##################################################################################
   }
@@ -71,4 +71,3 @@ data.export<-do.call(rbind.data.frame, list.indices)
 return(data.export)
 
 }
- 
