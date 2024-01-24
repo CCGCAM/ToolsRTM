2e45d3320a75767a931adb33a4ee5a032f7bef93
CCGCAM <camicar>
2024-01-24 16:38:25 +0100

    updates, fixed errors

--- R/getLUT_liberty.R
+++ R/getLUT_liberty.R
@@ -8,27 +8,27 @@
 #' @export
 #'
 #' @examples here adding examples ....
-#' 
+#'
 getLUT_liberty<-function(inputs=NULL, nLUT=100, setseed = 123){
-  
+
   if ( is.null(setseed)){
     set.seed(setseed)
   }
   set.seed(setseed)
-  
+
   if (dim(inputs)[2] != 10) {
     message(' Please provide a LUT table with 10 columns')
-    print('with theses columns and names: variable, lower,upper,units,Distribution,Mean_D,Std_D,Dependecies,include,default')
+    print('with theses columns and names: variable, lower,upper,units,Distribution,Mean_D,Std_D,Dependencies,use.default,default')
     message(' Please check the example')
-  
+
     stop()
-  } 
-  
+  }
+
   if (is.null(nLUT)){
     message('number of varitions for each input is fixed to 100')
     nLUT = 100
   }
-  
+
   var.list = list()
   inputs_names<- inputs[,'variable']
   for (i in  c(1:length(inputs_names))) {
@@ -36,24 +36,24 @@ getLUT_liberty<-function(inputs=NULL, nLUT=100, setseed = 123){
     trait <- inputs_names[i]
     #print(inputs_names[i])
     ## for Carotenoids
-    if (table.sb[,'Dependecies'] == 'Yes' ){
-      
+    if (table.sb[,'Dependencies'] == 'Yes' ){
+
       var.list[[trait]] <- 'NA'
       trait_dep <-trait
     }
-    
+
     if (table.sb[,'use.default'] == 1){
-      
-      
-      
+
+
+
       if (table.sb[,'Distribution'] == 'Uniform'){
-        
+
         var.list[[trait]] <- stats::runif(nLUT,min = table.sb[,'lower'],max=table.sb[,'upper'])
-        
+
       } else if (table.sb[,'Distribution'] == 'Gaussian'){
         n_casesNorm= 3 * nLUT
-        var.list[[trait]] <-ToolsRTM::gauss_byMin_Max(n=nLUT, m=as.numeric(table.sb[,'Mean_D']), 
-                                                      s=as.numeric(table.sb[,'Std_D']), lwr=table.sb[,'lower'], 
+        var.list[[trait]] <-simRTM::gauss_byMin_Max(n=nLUT, m=as.numeric(table.sb[,'Mean_D']),
+                                                      s=as.numeric(table.sb[,'Std_D']), lwr=table.sb[,'lower'],
                                                       upr=table.sb[,'upper'], nnorm=n_casesNorm)
       }
     } else{
@@ -62,8 +62,7 @@ getLUT_liberty<-function(inputs=NULL, nLUT=100, setseed = 123){
   }
   ### Get correlation for Car based on Cab
   LUT <- do.call(cbind,var.list )
-  
-  
+
+
   return(LUT)
 }
-  
