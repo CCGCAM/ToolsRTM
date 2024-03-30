3158903d22f489537b38e21ea5cea03534e1c430
CCGCAM <camicar>
2024-03-30 12:39:17 +0100

    updates

--- R/get.cars.pls.R
+++ R/get.cars.pls.R
@@ -1,4 +1,23 @@
-carspls<-function(X,y,nLV=2,fold=10,scale.pretreat=1,iteration=50,PartitionType="interleaved")
+#' Perform CARS-PLS analysis.
+#'
+#' This function performs Competitive Adaptive Reweighted Sampling (CARS) for partial least squares (PLS) analysis.
+#'
+#' @param X The sample matrix, where samples are in rows and variables are in columns.
+#' @param y The response variable.
+#' @param nLV The number of latent variables in PLS. Default is 2.
+#' @param fold The number of segments for cross-validation. Default is 10.
+#' @param scale.pretreat Whether to scale the variables. 1 for scaling, 0 for no scaling (only centered). Default is 1.
+#' @param iteration The number of Monte Carlo samplings in CARS. Default is 50.
+#' @param PartitionType The partition type for cross-validation: "random", "consecutive", or "interleaved". Default is "interleaved".
+#'
+#' @return A list containing the results of the CARSPLS analysis.
+#' @export
+#'
+#' @examples
+#' carspls(X, y)
+#' carspls(X, y, nLV = 3, fold = 5, scale.pretreat = 0)
+#'
+get.cars.pls<-function(X,y,nLV=2,fold=10,scale.pretreat=1,iteration=50,PartitionType="interleaved")
 {
 #+++ CARS: Competitive Adaptive Reweighted Sampling method for
 #+++ X: Sample matrix, sample in row and variable in column.
